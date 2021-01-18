283e5bd6c39dabda7b6ddd52462a005f48e8a67e
danielhuri <danielhuri8@gmail.com>
2021-01-18 22:20:04 +0200

    create feature vector

--- algorithms/imageContentSearch.py
+++ algorithms/imageContentSearch.py
@@ -25,6 +25,35 @@ class ImageDescriptor:
         return mahotas.features.zernike_moments(image, self.radius)
 
 
+# Create a thresholded image
+def getImageMask(imagePath):
+    image = cv2.imread(imagePath)
+    # convert it to grayscale
+    image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    # pad the image with extra white pixels to ensure the
+    # edges of the image are not up against the borders
+    # of the image
+    image = cv2.copyMakeBorder(image, 15, 15, 15, 15,
+                               cv2.BORDER_CONSTANT, value=255)
+
+    return image
+
+
+def getImageOutline(maskedImage):
+    # invert the image and threshold it
+    thresh = cv2.bitwise_not(maskedImage)
+    thresh[thresh > 0] = 255
+
+    outline = np.zeros(maskedImage.shape, dtype="uint8")
+    cnts = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL,
+                            cv2.CHAIN_APPROX_SIMPLE)
+    cnts = imutils.grab_contours(cnts)
+    cnts = sorted(cnts, key=cv2.contourArea, reverse=True)[0]
+    cv2.drawContours(outline, [cnts], -1, 255, -1)
+
+    return outline
+
+
 def indexDataset(sourcePath, resultPath):
     # Variable Definition
     resultIndexes = {}
@@ -33,17 +62,12 @@ def indexDataset(sourcePath, resultPath):
     descriptor = ImageDescriptor(21)
 
     for imagePath in list_images(sourcePath):
-        image = cv2.imread(imagePath)
-        # convert it to grayscale
-        image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-        # pad the image with extra white pixels to ensure the
-        # edges of the image are not up against the borders
-        # of the image
-        image = cv2.copyMakeBorder(image, 15, 15, 15, 15,
-                                   cv2.BORDER_CONSTANT, value=255)
-        # invert the image and threshold it
-        thresh = cv2.bitwise_not(image)
-        thresh[thresh > 0] = 255
+        imageName = ntpath.basename(imagePath)
+        image = getImageMask(imagePath)
+        outlilne = getImageOutline(image)
+
+        resultIndexes[imageName] = descriptor.describeByShape(outlilne)
+        print(resultIndexes[imageName].shape)
 
 
 dirname = os.path.dirname(__file__)
