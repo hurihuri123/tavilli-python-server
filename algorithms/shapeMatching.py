9121b95ebf42261117648d6200e1c29d290a4cab
danielhuri <danielhuri8@gmail.com>
2021-01-30 18:48:26 +0200

    loop

--- algorithms/shapeMatching.py
+++ algorithms/shapeMatching.py
@@ -3,6 +3,23 @@ import cv2
 from matplotlib import pyplot as plt
 import os
 import pickle
+from imutils.paths import list_images
+import ntpath
+
+
+def showImage(imPath, name="result-image"):
+    image = cv2.imread(os.path.join(dirPath, imPath))
+    image = cv2.resize(image, (600, 600))
+    cv2.imshow(name, image)
+
+
+def showResults(queryPath, results):
+    showImage(queryPath, "queryImage")
+    cv2.waitKey(0)
+    for result in results:
+        print("Match with {} perecentage {}".format(result[1], result[0]))
+        showImage(result[1])
+        cv2.waitKey(0)
 
 
 def drawCon(contours, image, name="name"):
@@ -17,43 +34,51 @@ def match(contours, contours2):
     distance = cv2.matchShapes(
         contours[0], contours2[0], cv2.CONTOURS_MATCH_I1, 0)
 
-    print("matchShape: {} , Hausdor: {} , ShapeContext: ".format(
-        distance, distanceHausdor))
+    return distanceHausdor
+
+
+def getImageContur(imagePath):
+    im1 = cv2.imread(imagePath)
+
+    queryImage = cv2.cvtColor(im1, cv2.COLOR_BGR2GRAY)
+
+    # apply erosion
+    kernel = np.ones((2, 2), np.uint8)
+    originalErosion = cv2.erode(queryImage, kernel, iterations=1)
+
+    # retrieve edges with Canny
+    thresh = 175
+    originalEdges = cv2.Canny(originalErosion, thresh, thresh*2)
+    contours, hierarchy = cv2.findContours(
+        originalEdges, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+    return contours
 
 
 dirname = os.path.dirname(__file__)
 dirPath = os.path.join(dirname, "testImages")
 queryImagePath = os.path.join(
     dirPath, "identical1.jpg")
-queryImagePath2 = os.path.join(
-    dirPath, "identical3.jpg")
 
-im1 = cv2.imread(queryImagePath)
-im2 = cv2.imread(queryImagePath2)
 
-# make them gray
-originalGray = cv2.cvtColor(im1, cv2.COLOR_BGR2GRAY)
-drawnGray = cv2.cvtColor(im2, cv2.COLOR_BGR2GRAY)
-
-# apply erosion
-kernel = np.ones((2, 2), np.uint8)
-originalErosion = cv2.erode(originalGray, kernel, iterations=1)
-drawnErosion = cv2.erode(drawnGray, kernel, iterations=1)
-
-# retrieve edges with Canny
-thresh = 175
-originalEdges = cv2.Canny(originalErosion, thresh, thresh*2)
-drawnEdges = cv2.Canny(drawnErosion, thresh, thresh*2)
-
-# extract contours
-contours, hierarchy = cv2.findContours(
-    originalEdges, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-
-contours2, hierarchy2 = cv2.findContours(
-    drawnEdges, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-
-drawCon(contours, im1, name="image1")
-drawCon(contours2, im2, name="image2")
+queryConturs = getImageContur(queryImagePath)
+queryImage = cv2.imread(queryImagePath)
+drawCon(queryConturs, queryImage, name="image1")
 cv2.waitKey()
 
-match(contours, contours2)
+
+results = {}
+for imagePath in list_images(dirPath):
+    imageName = ntpath.basename(imagePath)
+    currentConturs = getImageContur(imagePath)
+
+    # currentImage = cv2.imread(imagePath)
+    # drawCon(currentConturs, currentImage, name="image2")
+
+    distance = match(currentConturs, queryConturs)
+    # print("distance is {}".format(distance))
+    # cv2.waitKey()
+    results[imageName] = distance
+
+
+results = sorted([(v, k) for (k, v) in results.items()])
+showResults(queryImagePath, reversed(results))
