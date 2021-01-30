a788d5161abd6b0a67b1d1972f606b36c589105e
danielhuri <danielhuri8@gmail.com>
2021-01-30 12:27:09 +0200

    template match layout

--- algorithms/templateMatch.py
+++ algorithms/templateMatch.py
@@ -1,11 +1,27 @@
 import cv2
-import numpy as np
-# from matplotlib import pyplot as plt
+import os
+import ntpath
+from imutils.paths import list_images
 
 
-def templateMatch(sourceImagePath, templateImagePath):
-    img_rgb = cv2.imread(sourceImagePath)
-    img_gray = cv2.cvtColor(img_rgb, cv2.COLOR_BGR2GRAY)
-    template = cv2.imread(templateImagePath, 0)
+def getFeature(imagePath):
+    return {}
 
-    return
+
+dirname = os.path.dirname(__file__)
+dirPath = os.path.join(dirname, "testImages")
+queryImagePath = os.path.join(dirPath, "ball.jpg")
+resultIndex = os.path.join(dirname, "imagesIndexes")
+
+resultIndexes = {}
+for imagePath in list_images(dirPath):
+    imageName = ntpath.basename(imagePath)
+    resultIndexes[imageName] = getFeature(imagePath)
+
+
+results = {}
+for(k, features) in resultIndex.items():
+    results[k] = -1
+
+results = sorted([(v, k) for (k, v) in results.items()])
+print(results)
