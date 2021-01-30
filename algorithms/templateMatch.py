10414750ebd467d3770ef7f2c1a50db16cfd92ea
danielhuri <danielhuri8@gmail.com>
2021-01-30 13:16:12 +0200

    MSE

--- algorithms/templateMatch.py
+++ algorithms/templateMatch.py
@@ -2,18 +2,34 @@ import cv2
 import os
 import ntpath
 from imutils.paths import list_images
+import numpy as np
+# from skimage.measure import structural_similarity as ssim
 
 
-# TODO - wrap this 2 with class that can store objects
+def mse(imageA, imageB):
+    # the 'Mean Squared Error' between the two images is the
+    # sum of the squared difference between the two images;
+    # NOTE: the two images must have the same dimension
+    height, width = imageA.shape
+    imageB = cv2.resize(imageB, (width, height))
+    diff = cv2.absdiff(imageA, imageB)
+
+    # return the MSE, the lower the error, the more "similar"
+    # the two images are
+    return diff.sum()
+
+
 class Features():
     def __init__(self):
         super().__init__()
 
     def getFeature(self, imagePath):
-        return {}
+        image = cv2.imread(imagePath)
+        image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+        return image
 
     def compareFeatures(self, feature1, feature2):
-        return 100
+        return mse(feature1, feature2)
 
 
 class Search():
@@ -40,7 +56,7 @@ class Search():
 if __name__ == "__main__":
     dirname = os.path.dirname(__file__)
     dirPath = os.path.join(dirname, "testImages")
-    queryImagePath = os.path.join(dirPath, "ball.jpg")
+    queryImagePath = os.path.join(dirPath, "identical1.jpg")
 
     features = Features()
     searcher = Search(features.getFeature, features.compareFeatures)
