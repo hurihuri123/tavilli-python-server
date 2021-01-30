ce906076f6ede834d10279406be3519487b0d601
danielhuri <danielhuri8@gmail.com>
2021-01-30 12:56:13 +0200

    classes

--- algorithms/templateMatch.py
+++ algorithms/templateMatch.py
@@ -4,12 +4,37 @@ import ntpath
 from imutils.paths import list_images
 
 
-def getFeature(imagePath):
-    return {}
+# TODO - wrap this 2 with class that can store objects
+class Features():
+    def __init__(self):
+        super().__init__()
+
+    def getFeature(self, imagePath):
+        return {}
+
+    def compareFeatures(self, feature1, feature2):
+        return 100
 
 
-def compareFeatures(feature1, feature2):
-    return 100
+class Search():
+    def __init__(self, getFeature, compareFeatures):
+        super().__init__()
+        self.getFeature = getFeature
+        self.compareFeatures = compareFeatures
+
+    def match(self, queryPath):
+        resultIndexes = {}
+        for imagePath in list_images(dirPath):
+            imageName = ntpath.basename(imagePath)
+            resultIndexes[imageName] = self.getFeature(imagePath)
+
+        queryFeatures = self.getFeature(queryPath)
+        results = {}
+        for(k, features) in resultIndexes.items():
+            results[k] = self.compareFeatures(features, queryFeatures)
+
+        results = sorted([(v, k) for (k, v) in results.items()])
+        return results
 
 
 if __name__ == "__main__":
@@ -17,15 +42,6 @@ if __name__ == "__main__":
     dirPath = os.path.join(dirname, "testImages")
     queryImagePath = os.path.join(dirPath, "ball.jpg")
 
-    resultIndexes = {}
-    for imagePath in list_images(dirPath):
-        imageName = ntpath.basename(imagePath)
-        resultIndexes[imageName] = getFeature(imagePath)
-
-    queryFeatures = getFeature(queryImagePath)
-    results = {}
-    for(k, features) in resultIndexes.items():
-        results[k] = compareFeatures(features, queryFeatures)
-
-    results = sorted([(v, k) for (k, v) in results.items()])
-    print(results)
+    features = Features()
+    searcher = Search(features.getFeature, features.compareFeatures)
+    print(searcher.match(queryImagePath))
