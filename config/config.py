7b08fbc4418bb533f46744fa6c043b5bb828cf4e
danielhuri <danielhuri8@gmail.com>
2021-02-02 20:23:28 +0200

    add images path config

--- config/config.py
+++ config/config.py
@@ -1,10 +1,21 @@
+
 if(True):
     DATABASE_HOST = "localhost"
     DATABASE_USERNAME = "root"
     DATEBASE_PASSWORD = ""
     DATABASE_NAME = "tavilli"
+
+    IMAGES_HOST = "https://prod-images.tavilli.co.il"
+
 else:
     DATABASE_HOST = "database-2.cxekfsj5gkic.eu-central-1.rds.amazonaws.com"
     DATABASE_USERNAME = "admin"
     DATEBASE_PASSWORD = "tavilI123"
     DATABASE_NAME = "tavilli"
+
+    IMAGES_HOST = "http://localhost:8443"
+
+
+IMAGES_FOLDER = IMAGES_HOST + "/images"
+OFFERS_IMAGES_FOLDER = IMAGES_HOST + "/offers"
+REQUESTS__IMAGES_FOLDER = IMAGES_HOST + "/requests"
