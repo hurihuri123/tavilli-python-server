13cb2f977f1285d18371ecbf285beb542364c1ab
danielhuri <danielhuri8@gmail.com>
2021-02-02 20:47:10 +0200

    Update config

--- config/config.py
+++ config/config.py
@@ -5,17 +5,16 @@ if(True):
     DATEBASE_PASSWORD = ""
     DATABASE_NAME = "tavilli"
 
-    IMAGES_HOST = "https://prod-images.tavilli.co.il"
-
+    IMAGES_HOST = "http://localhost:8443"
 else:
     DATABASE_HOST = "database-2.cxekfsj5gkic.eu-central-1.rds.amazonaws.com"
     DATABASE_USERNAME = "admin"
     DATEBASE_PASSWORD = "tavilI123"
     DATABASE_NAME = "tavilli"
 
-    IMAGES_HOST = "http://localhost:8443"
+    IMAGES_HOST = "https://prod-images.tavilli.co.il"
 
 
 IMAGES_FOLDER = IMAGES_HOST + "/images"
-OFFERS_IMAGES_FOLDER = IMAGES_HOST + "/offers"
-REQUESTS__IMAGES_FOLDER = IMAGES_HOST + "/requests"
+OFFERS_IMAGES_FOLDER = IMAGES_FOLDER + "/offers"
+REQUESTS__IMAGES_FOLDER = IMAGES_FOLDER + "/requests"
