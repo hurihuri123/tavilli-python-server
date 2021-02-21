2540678efd1d58db9ac19efdf70feb43d93bc626
danielhuri <danielhuri8@gmail.com>
2021-02-21 22:09:20 +0200

    convert image distance to percentage

--- algorithms/match.py
+++ algorithms/match.py
@@ -23,12 +23,43 @@ class Match(object):
     def title(self):
         return 100
 
+    """        
+        Args:
+            distance: images distance value (lower distance means better match percentage)
+            start_range: start value of distance's percentage group
+            end_range: end value of distance's percentage group
+            min_percentage: minimum percentage for distance's percentage group
+            max_percentage: maximum percentage for distance's percentage group
+        Returns:
+            Match percentage in range 0 to 100
+    """
+    @staticmethod
+    def convertImageDistanceToPercentage(distance, start_range, end_range, min_percentage, max_percentage):
+        all_way = end_range - start_range
+        traveled = distance - start_range
+        way_passed_percentage = traveled / all_way
+        return min_percentage + way_passed_percentage * (max_percentage - min_percentage)
+
     @property
     def images(self):
         match_percentage = None
         if self.images_distance:
-            # If negative then zero
-            match_percentage = max(0, 1 - self.images_distance) * 100
+            if self.images_distance > 1:  # Distance above 1 result with 0% match
+                match_percentage = 0
+            elif self.images_distance >= 0.90:  # Distance 0.90 - 1 result with 80-85% match
+                match_percentage = self.convertImageDistanceToPercentage(
+                    self.images_distance, 0.90, 1, 80, 85)
+            elif self.images_distance >= 0.70:  # Distance 0.70 - 0.90 result with 85-90% match
+                match_percentage = self.convertImageDistanceToPercentage(
+                    self.images_distance, 0.70, 0.90, 85, 90)
+            elif self.images_distance >= 0.50:  # Distance 0.50 - 0.70 result with 90-95% match
+                match_percentage = self.convertImageDistanceToPercentage(
+                    self.images_distance, 0.50, 0.70, 90, 95)
+            elif self.images_distance >= 0.20:  # Distance 0.20 - 0.50 result with 90-95% match
+                match_percentage = self.convertImageDistanceToPercentage(
+                    self.images_distance, 0.20, 0.50, 95, 100)
+            else:
+                matchPercantage = 100  # Distance 0-0.20 result with 100% match
 
         return match_percentage
 
