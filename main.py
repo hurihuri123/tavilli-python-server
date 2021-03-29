ca64b8c13afcd3ff7b4ea2a5b62c89b70ba47059
danielhuri <danielhuri8@gmail.com>
2021-03-29 14:03:01 +0300

    more logs

--- main.py
+++ main.py
@@ -102,9 +102,11 @@ class WebServerHandler(BaseHTTPRequestHandler):
         items = self.matcher.database.executeQuery(
             select_query(item_id))
         if items is None or len(items) != 1:
-            print("Query is: {}".format(select_query(item_id)))
+            all_items = self.matcher.database.executeQuery(
+                "SELECT id FROM OFFERS")
+            print("all items {}".format(all_items))
             LoggerService.error(
-                "Item with ID {} was not found {}".format(item_id))
+                "Item with ID {} was not found".format(item_id))
             return self.notFoundResponse()
         # Search matches for item
         try:
